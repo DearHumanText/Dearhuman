60267b2afb87838a6a45410c9ddbb9c18b7262c2
DearHumanText <dearhumantext@gmail.com>
2025-08-24 08:52:32 -0500

    Update blobs-health.js

--- netlify/functions/blobs-health.js
+++ netlify/functions/blobs-health.js
@@ -1,7 +1,10 @@
 const { getStore } = require('@netlify/blobs');
 
 exports.handler = async () => {
-  const store = getStore('dearhuman-stash'); // no siteID/token here
+  const store = getStore('dearhuman-stash', {
+    siteID: process.env.NETLIFY_SITE_ID,
+    token: process.env.NETLIFY_API_TOKEN,
+  });
   await store.set('health', 'ok');
   const val = await store.get('health', { type: 'text' });
   return {
